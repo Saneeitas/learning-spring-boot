3244a7c7721fd14b1bd64e79838a7a98b99bf711
Your Name <muhdsanee87@gmail.com>
2024-05-13 22:59:47 +0100

    debug

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,27 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
 
 @RestController
 public class DemoController {
 
+    private UserService userService;
+
+    public DemoController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "Home Page";
     }
 
@@ -21,5 +34,15 @@ public class DemoController {
     public String greeting(@RequestParam(value = "user", defaultValue = "World") String user) {
         return "Hello, " + user + "!";
     }
-    
+
+    @GetMapping("/users") // Get all users
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping("/users") // Create a user
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
 }
