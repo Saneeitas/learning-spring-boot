09693a87fcc8adec5995b2430a426bcfe9a501c6
Your Name <muhdsanee87@gmail.com>
2024-05-13 13:33:17 +0100

    create greeting routes

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,19 +1,25 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Home Page";
+    }
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
     }
 
-    @GetMapping("/")
-    public String home(){
-        return "Home Page";
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "user", defaultValue = "World") String user) {
+        return "Hello, " + user + "!";
     }
     
 }
